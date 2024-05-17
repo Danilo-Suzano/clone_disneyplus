0506df2b9648cbd61cb668bfd166d5b58ec6afd2
Danilo <123773212+Danilo-Suzano@users.noreply.github.com>
2024-05-17 15:56:52 -0300

    criando e configurando os shows

---
+++ src/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', function(button){
+            const tabTarget = button.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${tabTarget}]`);
+            hideAllAbs();
+            tab.classList.add('shows__list--is-active');
+            removeActiveButton();
+            button.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+})
+
+function removeActiveButton(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function hideAllAbs(){
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for(let i = 0; i < tabsContainer.length; i++){
+        tabsContainer[i].classList.remove('shows__list--is-active')
+    }
+}
